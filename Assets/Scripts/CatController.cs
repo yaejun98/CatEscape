97f6ba8bdf3868c99a7db845574cae2d8c07da98
yjshin <shinyj0548@gmail.com>
2024-01-31 18:31:33 +0900

    플레이어의 이동거리를 제한하기 위해 플레이어의 좌표 출력하는 코드 추가

--- Assets/Scripts/CatController.cs
+++ Assets/Scripts/CatController.cs
@@ -1,38 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Assertions.Comparers;
+using UnityEngine.SceneManagement;
 
 public class CatController : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D rbody;
     [SerializeField] private float moveForce = 100f;
     [SerializeField] private float jumpForce = 680f;
-    private Animator anim;
+
     [SerializeField]
     private ClimbCloudGameDirector gameDirector;
 
-    [SerializeField] private float speed = 1f; 
+    private Animator anim;
+
     private void Start()
     {
-        // this.gameObject.GetComponent<Animation>();
-        this.anim = this.GetComponent<Animator>(); // this 생략 가능
-        //anim.speed = moveForce * Time.deltaTime; 
+        //this.gameObject.GetComponent<Animation>();
+
+        anim = GetComponent<Animator>();
 
         //this.gameDirector = GameObject.Find("GameDirector").GetComponent<ClimbCloudGameDirector>();
         //this.gameDirector = GameObject.FindAnyObjectByType<ClimbCloudGameDirector>();
-        // ? 
-
+   
     }
 
     void Update()
     {
+        
         //스페이스바를 누르면 
-        //if (Input.GetKeyDown(KeyCode.Space))
-        //{
-        //    //힘을 가한다 
-        //    this.rbody.AddForce(this.transform.up * this.force);
-        //    //this.rbody.AddForce(Vector3.up * this.force);
-        //}
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //힘을 가한다 
+            this.rbody.AddForce(this.transform.up * this.jumpForce);
+            //this.rbody.AddForce(Vector3.up * this.force);
+        }
 
         // -1, 0, 1 : 방향 
         int dirX = 0;
@@ -40,7 +44,6 @@ public class CatController : MonoBehaviour
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             dirX = -1;
-            
         }
 
         if (Input.GetKey(KeyCode.RightArrow))
@@ -48,16 +51,18 @@ public class CatController : MonoBehaviour
             dirX = 1;
         }
 
-        Debug.Log(dirX); //방향 -1,0,1
-        //스케일 X를 변경하는데 키가 눌렸을 때만
-        // 키가 눌렸을 때만 == (dirX != 0)
-        if (dirX != 0) // if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow))
+        // Debug.Log(dirX); //방향 -1, 0, 1
+
+        //스케일 X를 변경 하는데 키가 눌렸을 때만 
+        //키가 눌렸을때만 = (dirX != 0)
+        if (dirX != 0)
         {
-            this.transform.localScale = new Vector3(dirX, 1, 1); //움직일때 모양 변경
+            this.transform.localScale = new Vector3(dirX, 1, 1);
         }
+
+
         //벡터의 곱 
-        ///////////
-        Debug.Log(this.transform.right * dirX);  //벡터3
+        //Debug.Log(this.transform.right * dirX);  //벡터3
 
         //도전 ! : 속도를 제한하자 
         //velocity.x 가 3정도가 넘어가니깐 빨라지는거 같드라구...
@@ -66,8 +71,31 @@ public class CatController : MonoBehaviour
             this.rbody.AddForce(this.transform.right * dirX * moveForce);
         }
 
-        anim.speed = Mathf.Abs(rbody.velocity.x) / speed; 
-
+        this.anim.speed = (Mathf.Abs(this.rbody.velocity.x) / 2f);
         this.gameDirector.UpdateVelocityText(this.rbody.velocity);
+
+        
+        // Debug.Log(this.transform.position);
+        
+        
+
+
     }
+
+    // Trigger 모드 일경우 충돌 판정을 해주는 이벤트 함수
+    private bool hasEntered = false;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {   
+        if (!hasEntered)
+        {
+            Debug.LogFormat("OnTriggerEnter2D: {0}", collision);
+            SceneManager.LoadScene("ClimbCloudClear");
+            hasEntered = true; // 한 번 이벤트가 발생하면 이 변수를 true로 설정하여 두 번 이상 호출되지 않도록 함
+           
+        }
+       
+    }
+    
+   
+
 }
