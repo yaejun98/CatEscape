d89eb2f677e74899b2099acf0993a30bbcf2bfb9
yjshin <shinyj0548@gmail.com>
2024-02-01 18:36:08 +0900

    Alpha0 과 1을 눌렀을때 애니메이터를 연결시켜 동작 바꾸기

--- Assets/Scripts/BombGuyController.cs
+++ Assets/Scripts/BombGuyController.cs
@@ -18,32 +18,38 @@ public class BombGuyController : MonoBehaviour
         //Transform animTransform = this.transform.Find("anim");
         //GameObject animGo = animTransform.gameObject;
         //this.anim = animGo.GetComponent<Animator>();
+
         //코루틴 함수 호출시
-        this.StartCoroutine(this.CoMove());
+        //this.StartCoroutine(this.CoMove());
     }
 
-    IEnumerator CoMove()
-    {
-        //매 프레임마다 앞으로 이동
-        while (true)
-        {
-            this.transform.Translate(transform.right * 1f * Time.deltaTime);
-            yield return null; //다음 프레임으로 넘어간다 => while문 안에 들어가야 함
-        }
-    }
+    //IEnumerator CoMove()
+    //{
+    //    //매 프레임마다 앞으로 이동
+    //    while (true)
+    //    {
+    //        this.transform.Translate(transform.right * 1f * Time.deltaTime);
+    //        yield return null; //다음 프레임으로 넘어간다 => while문 안에 들어가야 함
+    //    }
+    //}
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Alpha0))
+            //Alpha+num 은 키보드 위에 숫자버튼
+            //Keypad+num 은 키보드 우측 숫자버튼
         {
             Debug.Log("Idle");
             //애니메이션 전환 하기
             //전환 할 때 파라미터에 값을 변경하기
+            this.anim.SetInteger("State", 0);
 
         }
         if (Input.GetKeyDown(KeyCode.Alpha1))
         {
             Debug.Log("Run");
+            this.anim.SetInteger("State", 1);
+
         }
 
     }
