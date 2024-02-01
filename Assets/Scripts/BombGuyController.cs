7244be04cf8ccdc57724da70f6dc380c055f65ab
yjshin <shinyj0548@gmail.com>
2024-02-01 18:09:28 +0900

    Pirate Bomb게임 생성 후 코루틴 작성

---
+++ Assets/Scripts/BombGuyController.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombGuyController : MonoBehaviour
+{
+    //BombGuyController가 Animator 컴포넌트를 알아야한다
+    //왜? 애니메이션 전환을 해야 해서
+    //Animator 컴포넌트는 자식 오브젝트 anim에 붙어 있다
+    //어떻게 하면 자식오브젝트에 붙어 있는 Animator 컴포넌트를 가져올 수 있을까
+    [SerializeField] private Animator anim;
+    
+    private void Start()
+    {
+
+        // [SerializeField] Animator anim; 를 쓰지 않는다면
+
+        //Transform animTransform = this.transform.Find("anim");
+        //GameObject animGo = animTransform.gameObject;
+        //this.anim = animGo.GetComponent<Animator>();
+        //코루틴 함수 호출시
+        this.StartCoroutine(this.CoMove());
+    }
+
+    IEnumerator CoMove()
+    {
+        //매 프레임마다 앞으로 이동
+        while (true)
+        {
+            this.transform.Translate(transform.right * 1f * Time.deltaTime);
+            yield return null; //다음 프레임으로 넘어간다 => while문 안에 들어가야 함
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha0))
+        {
+            Debug.Log("Idle");
+            //애니메이션 전환 하기
+            //전환 할 때 파라미터에 값을 변경하기
+
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            Debug.Log("Run");
+        }
+
+    }
+}
