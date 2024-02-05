d528e3e3f5c16b38fafa60fbc73918271cfb95c2
yjshin <shinyj0548@gmail.com>
2024-02-06 01:12:02 +0900

    AppleRetry 화면전환 오류 해결

--- Assets/Scripts/GameSceneMain.cs
+++ Assets/Scripts/GameSceneMain.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-public class GameSceneMain : MonoBehaviour
+public class GameSceneMains : MonoBehaviour
 {
     [SerializeField] private Text textHp;
     [SerializeField] private Button btnLoadScene;
